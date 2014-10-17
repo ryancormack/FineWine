f71dd49143e931860d47a52874065790f509636e
ryancormack <ryancormack89@gmail.com>
2014-10-17 22:18:58 +0100

    Added some stuff to make featuress work, a shiny global asax and Castle, because why not

---
+++ FineWine/FineWine/Global.asax.cs
@@ -0,0 +1,39 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using FineWine.Infrastructure.Factories;
+
+namespace FineWine
+{
+    public class MvcApplication : HttpApplication
+    {
+
+        static IWindsorContainer _container;
+
+        protected void Application_Start()
+        {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new FeatureViewLocationRazorViewEngine());
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            AreaRegistration.RegisterAllAreas();
+
+            BootstrapContainer();
+
+        }
+
+        private static void BootstrapContainer()
+        {
+            _container = new WindsorContainer().Install(FromAssembly.This());
+            var controllerFactory = new WindsorControllerFactory(_container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+        }
+
+        protected void Application_End()
+        {
+            _container.Dispose();
+        }
+    }
+}
