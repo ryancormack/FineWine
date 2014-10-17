732ac40d6c788c7a06849ab7a0c9e90060845f55
ryancormack <ryancormack89@gmail.com>
2014-10-17 22:39:47 +0100

    Suddenly a wild test project and controller appeared

---
+++ FineWine/FineWine.Web.Tests/ControllerTests/HomeControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using FineWine.Features.Home;
+using FluentAssertions;
+using Machine.Specifications;
+
+namespace FineWine.Web.Tests.ControllerTests
+{
+    public class HomeControllerTests
+    {
+        public class When_getting_the_home_page
+        {
+            Because of = () =>
+            {
+                _result = _controller.Index();
+            };
+
+            It should_return_the_home_page = () =>
+            {
+                (_result as ViewResult).ViewName.Should().Be("Index");
+            };
+
+            Establish context = () =>
+            {
+                _controller = new HomeController();
+            };
+
+            private static ActionResult _result;
+            private static HomeController _controller;
+        }
+    }
+}
---
+++ FineWine/FineWine/Features/Home/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace FineWine.Features.Home
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
